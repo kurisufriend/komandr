a90f9704e4c0183f09a63415353dc00d098a96d7
BuildTools <buffer@sidekek.ml>
2022-06-11 23:24:56 -0400

    sandbox webui as json responses

--- plugins/dpt.py
+++ plugins/dpt.py
@@ -1,9 +1,12 @@
 depends = ["bprint", "meido"]
+from time import sleep
 def run(l):
-    pages = l['meido'].instance.get_catalog("g")
-    for page in pages:
-        for thread in page["threads"]:
-            if not thread.get("sub"): continue
-            if "/dpt/" in thread["sub"]:
-                l["sand"].box["dpt"] = thread["no"]
-                l["bprint"].p(f"found /dpt/! thread no: {thread['no']}")
+    while True:
+        pages = l['meido'].instance.get_catalog("g")
+        for page in pages:
+            for thread in page["threads"]:
+                if not thread.get("sub"): continue
+                if "/dpt/" in thread["sub"]:
+                    l["sand"].box["dpt"] = thread["no"]
+                    l["bprint"].p(f"found /dpt/! thread no: {thread['no']}")
+        sleep(60)
